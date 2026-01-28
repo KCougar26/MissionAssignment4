a04415b0c067d91f1882c399bea8dab11a99406b
KCougar26 <kerick5@byu.edu>
2026-01-28 09:42:42 -0700

    finished driver

--- Program.cs
+++ Program.cs
@@ -12,5 +12,55 @@
 Console.WriteLine("Hello, Welcome to a game of Tic-Tac-Toe!");
 
 //Create array
-string[] board = {"1","2","3","4","5","6","7","8","9"}
+string[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+//initialize class
+SupportTools st = new SupportTools();
+
+//game logic
+bool gameOver = false;
+string currentPlayer = "X";
+
+while (!gameOver)
+{
+  //prints board
+  st.PrintBoard(board);
+  
+  //ask a player for choice
+  Console.WriteLine("Please enter a number between 1 and 9 to select your desired square: ");
+  int choice = int.Parse(Console.ReadLine()) - 1;
+  
+  //update array 
+  board[choice] = currentPlayer;
+  
+  //check for winner
+  int result = st.Winner(board);
+  
+  //0 = no win, 1 = win, 2 = draw
+  if (result == 1)
+  {
+      Console.WriteLine("Congratulations! You won!");
+      gameOver = true; 
+  }
+  else if (result == 2)
+  {
+      Console.WriteLine("Congratulations! It's a tie!");
+      gameOver = true;
+  }
+  else
+  {
+      Console.WriteLine("Oh no! You lost... Play again!");
+      gameOver = false;
+  }
+  
+  //switch players
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  
+  
+  
+  
+}
+
+
+
 
